1f19056aa2a34f748b2b6eec234351239ed3b8b0
MuhammetAkıtıcı <muhammetyasara@gmail.com>
2022-09-28 16:52:44 +0300

    Initialize Project

---
+++ Space-Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public int spawnCount;
+    public float spawnWait;
+    public int startWait;
+    public float waveWait;
+
+    public Text scoreText;
+    public int score;
+    public Text restartText;
+    public Text gameOverText;
+    public Text quitText;
+
+    private bool gameOver;
+    private bool restart;
+    private bool quit;
+
+    private void Update()
+    {
+        if (restart == true)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(0);
+            }
+
+        }
+        if (quit == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+    IEnumerator SpawnValues()
+    {
+
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+
+            if (gameOver)
+            {
+                yield break;
+            }
+            for (int i = 0; i < spawnCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-3, 3), 0, 10);
+                Quaternion spawnRotation = Quaternion.identity;
+
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver == true)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                quitText.text = "Press 'Q' for Quit";
+                quit = true;
+            }
+        }
+    }
+
+    public void UpdateScore()
+    {
+        score += 10;
+        scoreText.text = "Score" + score;
+
+    }
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over";
+        gameOver = true;
+    }
+
+    void Start()
+    {
+        gameOverText.text = "";
+        restartText.text = "";
+        quitText.text = " ";
+        gameOver = false;
+        restart = false;
+        quit = false;
+        StartCoroutine(SpawnValues());
+    }
+
+
+}
---
+++ Space-Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xmin, xmax, zmin, zmax;
+}
+
+
+public class PlayerController : MonoBehaviour
+{
+     
+    Rigidbody physic;
+    AudioSource audioPlayer;
+
+    public Boundary boundary;
+
+    public GameObject shot;
+    public GameObject shotSpawn;
+
+    [SerializeField] int tilt;
+    [SerializeField] int speed;
+    [SerializeField] float nextFire;
+    [SerializeField] float fireRate;
+    void Start()
+    {
+        physic = GetComponent<Rigidbody>();
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.transform.position, shotSpawn.transform.rotation);
+            audioPlayer.Play();
+        }
+       
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        physic.velocity = movement * speed;
+
+        Vector3 position = new Vector3(
+            Mathf.Clamp(physic.position.x, boundary.xmin, boundary.xmax),
+            0,
+            Mathf.Clamp(physic.position.z, boundary.zmin, boundary.zmax));
+
+        physic.position = position;
+
+        physic.rotation = Quaternion.Euler(0, 0,physic.velocity.x * tilt);
+    }
+}
